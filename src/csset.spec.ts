7ad3ee741d4b4fdbd22081b10b96eeddd05c42be
David Luna <david.luna@dynatrace.com>
2020-10-29 00:01:00 +0100

    feat: 1st version of supersetOf working

--- src/csset.spec.ts
+++ src/csset.spec.ts
@@ -25,6 +25,35 @@ describe('Csset', () => {
 
   describe('supersetOf', () => {
     describe('without subsets', () => {
+      test('should return false if 1st set is more specific', () => {
+        const data = [
+          {
+            sel1: 'div > p + span > a#id ~ p',
+            sel2: '*',
+            expected: false,
+          },
+          {
+            sel1: 'div > a ~ p',
+            sel2: 'div > p',
+            expected: false,
+          },
+          {
+            sel1: '* > span#id > a',
+            sel2: 'span > a',
+            expected: false,
+          }
+        ];
+
+        data.forEach( d => {
+          const set1 = new Csset(d.sel1);
+          const set2 = new Csset(d.sel2);
+          const result = `${set1} ${operationSymbols.supersetOf} ${set2} => ${set1.supersetOf(set2)}`;
+          const expected = `${set1} ${operationSymbols.supersetOf} ${set2} => ${d.expected}`;
+  
+          expect(result).toEqual(expected);
+        })
+      });
+
       test('should compare properly when the rule is less specific', () => {
         const data = [
           {
@@ -79,6 +108,183 @@ describe('Csset', () => {
           expect(result).toEqual(expected);
         })
       });
+
+      test('should compare properly for rules with different combinators', () => {
+        const data = [
+          {
+            sel1: 'div p',
+            sel2: 'div > p',
+            expected: true,
+          },
+          {
+            sel1: 'span ~ a',
+            sel2: 'span + a',
+            expected: true,
+          },
+          {
+            sel1: 'div > p',
+            sel2: 'div p',
+            expected: false,
+          },
+          {
+            sel1: 'span + a',
+            sel2: 'span ~ a',
+            expected: false,
+          },
+          {
+            sel1: 'div div p a',
+            sel2: 'div > div p > a',
+            expected: true,
+          },
+          {
+            sel1: 'div ~ span ~ a',
+            sel2: 'div + span + a',
+            expected: true,
+          },
+        ];
+        
+      
+        data.forEach( d => {
+          const set1 = new Csset(d.sel1);
+          const set2 = new Csset(d.sel2);
+          const result = `${set1} ${operationSymbols.supersetOf} ${set2} => ${set1.supersetOf(set2)}`;
+          const expected = `${set1} ${operationSymbols.supersetOf} ${set2} => ${d.expected}`;
+  
+          expect(result).toEqual(expected);
+        })
+      });
+
+      test('should compare properly with different hierarchy combinators', () => {
+        const data = [
+          {
+            sel1: 'section > article > h1 > p',
+            sel2: 'section article h1 p',
+            expected: false,
+          },
+          {
+            sel1: 'section article h1 p',
+            sel2: 'section > article > h1 > p',
+            expected: true,
+          },
+          {
+            sel1: 'section article h1 p',
+            sel2: 'section > article h1 p',
+            expected: true,
+          },
+          {
+            sel1: 'section article h1 p',
+            sel2: 'section article > h1 p',
+            expected: true,
+          },
+          {
+            sel1: 'section article h1 p',
+            sel2: 'section article h1 > p',
+            expected: true,
+          },
+          {
+            sel1: 'section > article h1 p',
+            sel2: 'section article h1 > p',
+            expected: false,
+          },
+        ];
+        
+      
+        data.forEach( d => {
+          const set1 = new Csset(d.sel1);
+          const set2 = new Csset(d.sel2);
+          const result = `${set1} ${operationSymbols.supersetOf} ${set2} => ${set1.supersetOf(set2)}`;
+          const expected = `${set1} ${operationSymbols.supersetOf} ${set2} => ${d.expected}`;
+  
+          expect(result).toEqual(expected);
+        })
+      });
+
+      test('should compare properly with different sibling combinators', () => {
+        const data = [
+          {
+            sel1: 'section + article + h1 + p',
+            sel2: 'section ~ article ~ h1 ~ p',
+            expected: false,
+          },
+          {
+            sel1: 'section ~ article ~ h1 ~ p',
+            sel2: 'section + article + h1 + p',
+            expected: true,
+          },
+          {
+            sel1: 'section ~ article ~ h1 ~ p',
+            sel2: 'section + article h1 p',
+            expected: false,
+          },
+          {
+            sel1: 'section ~ article ~ h1 ~ p',
+            sel2: 'section article + h1 p',
+            expected: false,
+          },
+          {
+            sel1: 'section ~ article ~ h1 ~ p',
+            sel2: 'section article h1 + p',
+            expected: false,
+          },
+          {
+            sel1: 'section + article ~ h1 ~ p',
+            sel2: 'section ~ article ~ h1 + p',
+            expected: false,
+          },
+        ];
+        
+      
+        data.forEach( d => {
+          const set1 = new Csset(d.sel1);
+          const set2 = new Csset(d.sel2);
+          const result = `${set1} ${operationSymbols.supersetOf} ${set2} => ${set1.supersetOf(set2)}`;
+          const expected = `${set1} ${operationSymbols.supersetOf} ${set2} => ${d.expected}`;
+  
+          expect(result).toEqual(expected);
+        })
+      });
+
+      test('should compare properly with edge cases combinators', () => {
+        console.log('should compare properly with edge cases combinators');
+        
+        const data = [
+          {
+            sel1: 'section > article + h1 p > a',
+            sel2: 'section article h1 ~ p a',
+            expected: false,
+          },
+          {
+            sel1: 'section > article + h1 p > a',
+            sel2: 'section article h1 ~ p > a',
+            expected: false,
+          },
+          {
+            sel1: 'section > article + h1 p > a',
+            sel2: 'section > article h1 + p > a',
+            expected: false,
+          },
+          {
+            sel1: 'section + article + h1 + p + a',
+            sel2: 'section article h1 p > a',
+            expected: false,
+          },
+          {
+            sel1: 'p > section + article + h1 + p + a',
+            sel2: 'section article h1 div p > a',
+            expected: false,
+          },
+        ];
+        
+      
+        data.forEach( d => {
+          const set1 = new Csset(d.sel1);
+          const set2 = new Csset(d.sel2);
+          const result = `${set1} ${operationSymbols.supersetOf} ${set2} => ${set1.supersetOf(set2)}`;
+          const expected = `${set1} ${operationSymbols.supersetOf} ${set2} => ${d.expected}`;
+  
+          expect(result).toEqual(expected);
+        })
+      });
     });
 
     describe('with subsets in 1st selector', () => {
@@ -110,23 +316,28 @@ describe('Csset', () => {
           expect(result).toEqual(expected);
         })
       });
-  
-      test('should compare properly for rules with the same combinators', () => {
-  
+    });
+
+    describe('with subsets in 2nd selector', () => {
+      test('should be superset if it is for all subsets in 2nd selector', () => {
         const data = [
           {
-            sel1: 'div > p + span > a ~ p',
-            sel2: 'div > p + span > a#id ~ p',
+            sel1: 'div',
+            sel2: 'div, div.class1, div#id',
             expected: true,
           },
           {
-            sel1: 'div#id > p + span > a',
-            sel2: 'div > p + span > a',
+            sel1: '*',
+            sel2: 'section, span > a ~ p, article, nav.hidden',
+            expected: true,
+          },
+          {
+            sel1: 'div',
+            sel2: 'div, p, span',
             expected: false,
           }
         ];
-        
-      
+
         data.forEach( d => {
           const set1 = new Csset(d.sel1);
           const set2 = new Csset(d.sel2);
--- src/csset.ts
+++ src/csset.ts
@@ -11,11 +11,15 @@ enum CssCombinatorValues {
   NONE       = '',
 }
 
+const HierarchyCombinators = [CssCombinatorValues.DESCENDANT, CssCombinatorValues.CHILD];
+const SiblingCombinators = [CssCombinatorValues.SIBLING, CssCombinatorValues.ADJACENT];
+
+type CssRuleList = Array<{ rule: CssRule, combinator: CssCombinatorValues }>;
 
 export class Csset {
   // These properties are exclusive if one is set the other is undefined
-  rules: Array<{ rule: CssRule, combinator: CssCombinatorValues }> = [];
-  subsets: Csset[] = [];
+  rules  : CssRuleList = [];
+  subsets: Csset[]     = [];
 
   /**
    * Parses the given selector filing up its private properties with metadata
@@ -37,35 +41,21 @@ export class Csset {
   supersetOf(set: Csset): boolean {
     // Case 1: both do not contain subsets (list of rules with values)
     if (this.rules.length && set.rules.length) {
-      // more specific selector cannot be superset of one less specific
-      if (this.rules.length > set.rules.length) {
-        return false;
-      }
-
-      // iterate backwards to check
-      let indexOne = this.rules.length - 1;
-      let indexTwo = set.rules.length - 1;
-
-      while(indexOne >= 0) {
-        let ruleOne = this.rules[indexOne];
-        let ruleTwo = set.rules[indexTwo];
-        
-        if (!ruleOne.rule.supersetOf(ruleTwo.rule)) {
-          return false;
-        }
-        if (ruleOne.combinator !== ruleTwo.combinator) {
-
-        }
-
-        // everithing okay so decrease indexes
-        indexOne--;
-        indexTwo--;
-      }
-      return true;
+      return this.containsRule(this.rules.slice(), set.rules.slice());
     }
     // Case 2: both do contain subsets
     // Case 3: this does not & received Csset does
-    // Case 3: this does & received Csset does not
+    if (this.rules.length && set.subsets.length) {
+      let index = set.subsets.length;
+
+      while(index--) {
+        if (!this.supersetOf(set.subsets[index])) {
+          return false;
+        }
+      }
+      return true;
+    }
+    // Case 4: this does & received Csset does not
     if (this.subsets.length && set.rules.length) {
       let index = this.subsets.length;
 
@@ -95,6 +85,10 @@ export class Csset {
   }
 
 
+  /**
+   * Fills the list of rules with it's combinators
+   * @param selector the selector to parse
+   */
   private parseSelector(selector: string): void {
     const lexer = new CssSelectorLexer(selector);
     let rule = new CssRule();
@@ -124,6 +118,83 @@ export class Csset {
           throw new SyntaxError(`Unknown token ${token.values[0]} at position ${token.position}`);
       }
     }
+  }
+
+  private containsRule (containerRules: CssRuleList, contentRules: CssRuleList): boolean {
+    // Base case: container is empty (meaning we have checked all its rules)
+    if (containerRules.length === 0) {
+      return true;
+    }
+
+    // Base case: content is empty (meaning we have checked all its rules)
+    if (contentRules.length === 0) {
+      return false;
+    }
+
+    // Base case: container is more specific than content
+    if (containerRules.length > contentRules.length) {
+      return false;
+    }
+
+    // Base case: deepest container does not contain deepest content
+    let containerRule = containerRules[containerRules.length - 1];
+    let contentRule   = contentRules[contentRules.length - 1];
+
+    if (!containerRule.rule.supersetOf(contentRule!.rule)) {
+      return false;
+    }
+
+    // The container selector has sibling combinator and not the content selector
+    // meaning container is more specific
+    if (
+      SiblingCombinators.indexOf(containerRule.combinator) !== -1 &&
+      HierarchyCombinators.indexOf(contentRule.combinator) !== -1
+    ) {
+      return false;
+    }
+
+    // Align and reset
+    this.alignLists(containerRules, contentRules);
+    containerRule = containerRules.pop()!;
+    contentRule   = contentRules.pop()!;
+
+    // Check if container combinator is more specific that content
+    if (
+      containerRule.combinator === CssCombinatorValues.CHILD &&
+      contentRule.combinator   === CssCombinatorValues.DESCENDANT
+    ){
+      return false;
+    }
+    if (
+      containerRule.combinator === CssCombinatorValues.ADJACENT &&
+      contentRule.combinator   === CssCombinatorValues.SIBLING
+    ){
+      return false;
+    }
+
+    return this.containsRule(containerRules, contentRules);
+  }
+
+
+  private alignLists(listOne: CssRuleList, listTwo: CssRuleList) {
+    const oneHasHierarchy = HierarchyCombinators.indexOf(this.getCombinator(listOne)) !== -1;
+    const twoHasHierarchy = HierarchyCombinators.indexOf(this.getCombinator(listTwo)) !== -1;
+    const needAlignment   = oneHasHierarchy !== twoHasHierarchy;
+
+    if (needAlignment) {
+      const selectedList = oneHasHierarchy ? listTwo : listOne;
+
+      while (
+        SiblingCombinators.indexOf(this.getCombinator(selectedList)) !== -1 &&
+        selectedList.length !== 1
+      ) {
+        selectedList.pop();
+      }
+    }
+  }
+
+  private getCombinator (list: CssRuleList): CssCombinatorValues {
+    return list[list.length - 1].combinator;
   }
 
 }
