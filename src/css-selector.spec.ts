1de6befef65c9183e36bcccaa369f875d3223465
David Luna <luna.bistuer@gmail.com>
2021-10-06 13:08:35 +0200

    test: add reversed tests

--- src/css-selector.spec.ts
+++ src/css-selector.spec.ts
@@ -36,214 +36,223 @@ const runExpectations = (testSet: TestItem[], operation: string) => {
 };
 
 describe('CssSelector', () => {
-  test('should return false if 1st set is more specific', () => {
-    const data = [
-      {
-        sel1: 'div > p + span > a#id ~ p',
-        sel2: '*',
-        expected: false,
-      },
-      {
-        sel1: 'div > a ~ p',
-        sel2: 'div > p',
-        expected: false,
-      },
-      {
-        sel1: '* > span#id > a',
-        sel2: 'span > a',
-        expected: false,
-      }
-    ];
+  describe('supersetOf', () => {
+    const reverse = (data: any[]) => data.map(d => ({
+      sel1: d.sel2,
+      sel2: d.sel1,
+      expected: d.expected
+    }));
 
-    runExpectations(data, operationSymbols.supersetOf);
-  });
+    test('should return false if 1st set is more specific', () => {
+      const data = [
+        {
+          sel1: 'div > p + span > a#id ~ p',
+          sel2: '*',
+          expected: false,
+        },
+        {
+          sel1: 'div > a ~ p',
+          sel2: 'div > p',
+          expected: false,
+        },
+        {
+          sel1: '* > span#id > a',
+          sel2: 'span > a',
+          expected: false,
+        }
+      ];
 
-  test('should compare properly when the rule is less specific', () => {
-    const data = [
-      {
-        sel1: '*',
-        sel2: 'div > p + span > a#id ~ p',
-        expected: true,
-      },
-      {
-        sel1: 'span > a ~ p',
-        sel2: 'div > p + span > a#id ~ p',
-        expected: true,
-      },
-      {
-        sel1: 'span#id > a',
-        sel2: 'div > p + span > a',
-        expected: false,
-      }
-    ];
-
-    runExpectations(data, operationSymbols.supersetOf);
-  });
-
-  test('should compare properly for rules with the same combinators', () => {
-
-    const data = [
-      {
-        sel1: 'div > p + span > a ~ p',
-        sel2: 'div > p + span > a#id ~ p',
-        expected: true,
-      },
-      {
-        sel1: 'div#id > p + span > a',
-        sel2: 'div > p + span > a',
-        expected: false,
-      }
-    ];
-    
+      runExpectations(data, operationSymbols.supersetOf);
+      runExpectations(reverse(data), operationSymbols.subsetOf);
+    });
   
-    runExpectations(data, operationSymbols.supersetOf);
-  });
-
-  test('should compare properly for rules with different combinators', () => {
-    const data = [
-      {
-        sel1: 'div p',
-        sel2: 'div > p',
-        expected: true,
-      },
-      {
-        sel1: 'span ~ a',
-        sel2: 'span + a',
-        expected: true,
-      },
-      {
-        sel1: 'div > p',
-        sel2: 'div p',
-        expected: false,
-      },
-      {
-        sel1: 'span + a',
-        sel2: 'span ~ a',
-        expected: false,
-      },
-      {
-        sel1: 'div div p a',
-        sel2: 'div > div p > a',
-        expected: true,
-      },
-      {
-        sel1: 'div ~ span ~ a',
-        sel2: 'div + span + a',
-        expected: true,
-      },
-    ];
-    
+    test('should compare properly when the rule is less specific', () => {
+      const data = [
+        {
+          sel1: '*',
+          sel2: 'div > p + span > a#id ~ p',
+          expected: true,
+        },
+        {
+          sel1: 'span > a ~ p',
+          sel2: 'div > p + span > a#id ~ p',
+          expected: true,
+        },
+        {
+          sel1: 'span#id > a',
+          sel2: 'div > p + span > a',
+          expected: false,
+        }
+      ];
   
-    runExpectations(data, operationSymbols.supersetOf);
-  });
-
-  test('should compare properly with different hierarchy combinators', () => {
-    const data = [
-      {
-        sel1: 'section > article > h1 > p',
-        sel2: 'section article h1 p',
-        expected: false,
-      },
-      {
-        sel1: 'section article h1 p',
-        sel2: 'section > article > h1 > p',
-        expected: true,
-      },
-      {
-        sel1: 'section article h1 p',
-        sel2: 'section > article h1 p',
-        expected: true,
-      },
-      {
-        sel1: 'section article h1 p',
-        sel2: 'section article > h1 p',
-        expected: true,
-      },
-      {
-        sel1: 'section article h1 p',
-        sel2: 'section article h1 > p',
-        expected: true,
-      },
-      {
-        sel1: 'section > article h1 p',
-        sel2: 'section article h1 > p',
-        expected: false,
-      },
-    ];
-    
+      runExpectations(data, operationSymbols.supersetOf);
+      runExpectations(reverse(data), operationSymbols.subsetOf);
+    });
   
-    runExpectations(data, operationSymbols.supersetOf);
-  });
-
-  test('should compare properly with different sibling combinators', () => {
-    const data = [
-      {
-        sel1: 'section + article + h1 + p',
-        sel2: 'section ~ article ~ h1 ~ p',
-        expected: false,
-      },
-      {
-        sel1: 'section ~ article ~ h1 ~ p',
-        sel2: 'section + article + h1 + p',
-        expected: true,
-      },
-      {
-        sel1: 'section ~ article ~ h1 ~ p',
-        sel2: 'section + article h1 p',
-        expected: false,
-      },
-      {
-        sel1: 'section ~ article ~ h1 ~ p',
-        sel2: 'section article + h1 p',
-        expected: false,
-      },
-      {
-        sel1: 'section ~ article ~ h1 ~ p',
-        sel2: 'section article h1 + p',
-        expected: false,
-      },
-      {
-        sel1: 'section + article ~ h1 ~ p',
-        sel2: 'section ~ article ~ h1 + p',
-        expected: false,
-      },
-    ];
-    
+    test('should compare properly for rules with the same combinators', () => {
   
-    runExpectations(data, operationSymbols.supersetOf);
-  });
+      const data = [
+        {
+          sel1: 'div > p + span > a ~ p',
+          sel2: 'div > p + span > a#id ~ p',
+          expected: true,
+        },
+        {
+          sel1: 'div#id > p + span > a',
+          sel2: 'div > p + span > a',
+          expected: false,
+        }
+      ];
 
-  test('should compare properly with edge cases combinators', () => {
-    const data = [
-      {
-        sel1: 'section > article + h1 p > a',
-        sel2: 'section article h1 ~ p a',
-        expected: false,
-      },
-      {
-        sel1: 'section > article + h1 p > a',
-        sel2: 'section article h1 ~ p > a',
-        expected: false,
-      },
-      {
-        sel1: 'section > article + h1 p > a',
-        sel2: 'section > article h1 + p > a',
-        expected: false,
-      },
-      {
-        sel1: 'section + article + h1 + p + a',
-        sel2: 'section article h1 p > a',
-        expected: false,
-      },
-      {
-        sel1: 'p > section + article + h1 + p + a',
-        sel2: 'section article h1 div p > a',
-        expected: false,
-      },
-    ];
-    
+      runExpectations(data, operationSymbols.supersetOf);
+      runExpectations(reverse(data), operationSymbols.subsetOf);
+    });
   
-    runExpectations(data, operationSymbols.supersetOf);
-  });
+    test('should compare properly for rules with different combinators', () => {
+      const data = [
+        {
+          sel1: 'div p',
+          sel2: 'div > p',
+          expected: true,
+        },
+        {
+          sel1: 'span ~ a',
+          sel2: 'span + a',
+          expected: true,
+        },
+        {
+          sel1: 'div > p',
+          sel2: 'div p',
+          expected: false,
+        },
+        {
+          sel1: 'span + a',
+          sel2: 'span ~ a',
+          expected: false,
+        },
+        {
+          sel1: 'div div p a',
+          sel2: 'div > div p > a',
+          expected: true,
+        },
+        {
+          sel1: 'div ~ span ~ a',
+          sel2: 'div + span + a',
+          expected: true,
+        },
+      ];
 
+      runExpectations(data, operationSymbols.supersetOf);
+      runExpectations(reverse(data), operationSymbols.subsetOf);
+    });
+  
+    test('should compare properly with different hierarchy combinators', () => {
+      const data = [
+        {
+          sel1: 'section > article > h1 > p',
+          sel2: 'section article h1 p',
+          expected: false,
+        },
+        {
+          sel1: 'section article h1 p',
+          sel2: 'section > article > h1 > p',
+          expected: true,
+        },
+        {
+          sel1: 'section article h1 p',
+          sel2: 'section > article h1 p',
+          expected: true,
+        },
+        {
+          sel1: 'section article h1 p',
+          sel2: 'section article > h1 p',
+          expected: true,
+        },
+        {
+          sel1: 'section article h1 p',
+          sel2: 'section article h1 > p',
+          expected: true,
+        },
+        {
+          sel1: 'section > article h1 p',
+          sel2: 'section article h1 > p',
+          expected: false,
+        },
+      ];
+
+      runExpectations(data, operationSymbols.supersetOf);
+      runExpectations(reverse(data), operationSymbols.subsetOf);
+    });
+  
+    test('should compare properly with different sibling combinators', () => {
+      const data = [
+        {
+          sel1: 'section + article + h1 + p',
+          sel2: 'section ~ article ~ h1 ~ p',
+          expected: false,
+        },
+        {
+          sel1: 'section ~ article ~ h1 ~ p',
+          sel2: 'section + article + h1 + p',
+          expected: true,
+        },
+        {
+          sel1: 'section ~ article ~ h1 ~ p',
+          sel2: 'section + article h1 p',
+          expected: false,
+        },
+        {
+          sel1: 'section ~ article ~ h1 ~ p',
+          sel2: 'section article + h1 p',
+          expected: false,
+        },
+        {
+          sel1: 'section ~ article ~ h1 ~ p',
+          sel2: 'section article h1 + p',
+          expected: false,
+        },
+        {
+          sel1: 'section + article ~ h1 ~ p',
+          sel2: 'section ~ article ~ h1 + p',
+          expected: false,
+        },
+      ];
+
+      runExpectations(data, operationSymbols.supersetOf);
+      runExpectations(reverse(data), operationSymbols.subsetOf);
+    });
+  
+    test('should compare properly with edge cases combinators', () => {
+      const data = [
+        {
+          sel1: 'section > article + h1 p > a',
+          sel2: 'section article h1 ~ p a',
+          expected: false,
+        },
+        {
+          sel1: 'section > article + h1 p > a',
+          sel2: 'section article h1 ~ p > a',
+          expected: false,
+        },
+        {
+          sel1: 'section > article + h1 p > a',
+          sel2: 'section > article h1 + p > a',
+          expected: false,
+        },
+        {
+          sel1: 'section + article + h1 + p + a',
+          sel2: 'section article h1 p > a',
+          expected: false,
+        },
+        {
+          sel1: 'p > section + article + h1 + p + a',
+          sel2: 'section article h1 div p > a',
+          expected: false,
+        },
+      ];
+
+      runExpectations(data, operationSymbols.supersetOf);
+      runExpectations(reverse(data), operationSymbols.subsetOf);
+    });
+  });
 });
