34687af5281d88f4d4645d6511e8d7986b9aabba
David Luna <luna.bistuer@gmail.com>
2023-02-07 00:04:11 +0100

    chore: remove deprecated types

--- src/types.ts
+++ src/types.ts
@@ -1,22 +1,4 @@
 /* eslint-disable no-shadow */
-export enum CssTokenType {
-  Void,
-  Id,
-  Element,
-  Class,
-  Attribute,
-  Space,
-  Combinator,
-  Separator,
-  Unknown,
-}
-export interface CssToken {
-  type: CssTokenType;
-  values: string[];
-  position: number;
-  length: number;
-}
-
 export enum CssMatcherSymbol {
   Presence = '',
   Equal = '=',
