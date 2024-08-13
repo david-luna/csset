c1e4ab32694daef936368eab5601f87dedbc5f90
David Luna <david@David-MacBook-Pro.local>
2024-08-13 10:32:59 +0200

    chore: add more examples in demo

--- demo/steps/steps-intersection.js
+++ demo/steps/steps-intersection.js
@@ -11,4 +11,31 @@ export const STEPS_INTERSECTION = [
       return quadrantOne.intersection(diamond);
     },
   },
+  {
+    comment: 'Like unions you can calculate intersection for more than two sets',
+    code: () => {
+      const quadrantOne = new Csset('.quadrant-one');
+      const even = new Csset('[d-even=true]');
+      const diamond = new Csset('.diamond');
+
+      return quadrantOne.intersection(even).intersection(diamond);
+    },
+  },
+  {
+    comment: 'A bigger intersection',
+    code: () => {
+      const quadrantFour = new Csset('.quadrant-four');
+      const diamond = new Csset('.diamond');
+      let result = quadrantFour;
+
+      for (let i = 0; i < 50; i++) {
+        if (i % 2 === 0) {
+          const s = new Csset(`[d-col=${i}]`);
+          result = result.intersection(s);
+        }
+      }
+
+      return result.intersection(diamond);
+    },
+  },
 ];
